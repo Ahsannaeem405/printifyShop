240120dd0636080bd4667b6027b3f4b124242bdf
rehman101 <73483363+rehman101@users.noreply.github.com>
2021-09-04 12:02:41 +0500

    okay chnages

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -8,8 +8,6 @@
 
 @include('layout.head')
 
-
-
 <body>
 
 <div id="sprd-main" class="SprdMain" style="width: 100% !important;">
@@ -18,13 +16,46 @@
 
         <header id="sprd-header-container" class="sprd-header-container">
 
-
-                <div class="row mt-2" style="width: 100%;">
-                   <a style="margin-left: auto" href="{{url('/cart')}}" class="{{ Request::is('cart')? 'active' : '' }}"> <i style="font-size: 30px" class="fas fa-cart-plus"></i>
+                <div class="row mt-2 ml-0" style="width: 100%;">
+                   <a style="margin-left: auto;padding-right: 5px" href="{{url('/cart')}}" class="{{ Request::is('cart')? 'active' : '' }}"> <i style="font-size: 30px" class="fas fa-cart-plus"></i>
                    </a>
                 </div>
-            <div class="row" style="width: 100%;">
-                <a class="sprd-header__title sprd__headline text-center" href="">Rehman</a>
+            <div class="row p-0 m-0" style="width: 100%;">
+
+
+                <nav style="width: 100%" class="navbar navbar-expand-lg navbar-light bg-light d-lg-none d-md-block d-sm-block d-block">
+
+<div class="row">
+
+
+                    <div class="col-3 pr-0">
+                    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    </div>
+    <h2  style="text-align: center;position: absolute;left: 45%">Rehman</h2>
+
+
+
+
+</div>
+                        <div class="col-12">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item {{ Request::is('/')? 'active' : '' }}">
+                                <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item {{ Request::is('products')? 'active' : '' }}">
+                                <a class="nav-link" href="{{url('products')}}">Products </a>
+                            </li>
+
+                            <li class="nav-item {{ Request::is('about')? 'active' : '' }}">
+                                <a class="nav-link" href="{{url('about')}}">About </a>
+                            </li>
+                    </div>
+                </nav>
+
+                <a class="sprd-header__title sprd__headline text-center d-lg-block d-md-none d-sm-none d-none font-weight-bold" href="">Rehman</a>
             </div>
             <div id="sprd-navigation"
                  class="sprd-navigation sprd-bg-m1 sprd-c-s3 sprd-lbc-s3 sprd-navigation--hide-on-touchdevs">
@@ -89,3 +120,6 @@
 </body>
 
 </html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>

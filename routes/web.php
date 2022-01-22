ded68e016c16c4f4956a84c4e7f5b037ab9f7d05
ranamabdullah06 <93966698+ranamabdullah06@users.noreply.github.com>
2022-01-22 12:44:50 +0500

    ASD

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::domain('blog.' . env('APP_URL'))->group(function () {
+Route::domain('blog/' . env('APP_URL'))->group(function () {
 
 
     Route::get('/',[\App\Http\Controllers\clientController::class,'index']);

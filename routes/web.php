161ec2dd4a49ed4ad3422fac30a55ce439f74580
ranamabdullah06 <93966698+ranamabdullah06@users.noreply.github.com>
2022-01-22 12:51:49 +0500

    UU

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::domain('blog/' . env('APP_URL'))->group(function () {
+// Route::domain('blog.' . env('APP_URL'))->group(function () {
+Route::group(array('domain' => 'subdomain.http://printifyshop.herokuapp.com/public'), function () {
 
 
     Route::get('/',[\App\Http\Controllers\clientController::class,'index']);
